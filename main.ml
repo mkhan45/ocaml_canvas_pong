644ea840af97c23aa057b3a6322155a769ad9b4a
Mikail Khan <mikail@mikail-khan.com>
2022-12-17 02:12:22 -0500

    cleanup

--- main.ml
+++ main.ml
@@ -8,17 +8,20 @@ let (<.) = Float.(<)
 
 let _ = Random.self_init
 
+(* Initializes PIXI graphics for drawing primitives *)
 let graphics = 
     let graphics = new%js graphics () in
     let _ = graphics##beginFill 0xffffff
     in graphics
 
+(* Initializes PIXI application *)
 let app = 
     let app = new%js application object%js val width = 800.0; val height = 800.0 end in
     let _ = Dom.appendChild Dom_html.document##.body app##.view
     and _ = app##.stage##addChild(graphics) in
     app
 
+(* Stores pressed inputs, since JS only gives events *)
 type input = { left_pressed: bool
              ; right_pressed: bool
              ; a_pressed: bool
@@ -55,6 +58,8 @@ module Vector = struct
     end
 end
 open Vector.Ops
+
+(* Makes a sane random velocity *)
 let random_vel () =
     let rec attempt () = match Random.float 5.0 -. 2.5 with
     | r when Float.abs r <. 2.0 -> attempt ()
@@ -70,7 +75,7 @@ module Rect = struct
     let left rect = rect.pos.x
     let right rect = rect.pos.x +. rect.bounds.x
 
-    let overlap a b = List.for_all ~f:(phys_equal false) [
+    let overlaps a b = List.for_all ~f:(phys_equal false) [
         left a >. right b;
         right a <. left b;
         top a >. bottom b;
@@ -81,7 +86,6 @@ module Rect = struct
     let integrate { pos; bounds; vel } = { pos = pos <+> vel; bounds; vel }
 end
 
-let init_paddle_speed = 4.5
 module GameState = struct
     type t = { top_paddle: Rect.t; bottom_paddle: Rect.t; ball: Rect.t; paddle_speed: float }
 
@@ -94,41 +98,45 @@ module GameState = struct
         ()
 
     let updated { top_paddle; bottom_paddle; ball; paddle_speed } =
+        let ball_hit_paddle = Rect.overlaps top_paddle ball || Rect.overlaps bottom_paddle ball
+        and point_scored = Rect.top ball <. 0.0 || Rect.bottom ball >. 800.0 in
         { top_paddle = top_paddle
-                       |> Rect.integrate
-                       |> (fun paddle -> match !input_state with
-                       | { left_pressed = true; right_pressed = false; _ } -> 
-                               { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
-                       | { left_pressed = false; right_pressed = true; _ } -> 
-                               { paddle with vel = { paddle.vel with x = paddle_speed } }
-                       | _ -> { paddle with vel = { x = 0.0; y = 0.0 }})
+          |> Rect.integrate
+          |> (fun paddle -> match !input_state with
+          | { left_pressed = true; right_pressed = false; _ } -> 
+                { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
+          | { left_pressed = false; right_pressed = true; _ } -> 
+                { paddle with vel = { paddle.vel with x = paddle_speed } }
+          | _ -> { paddle with vel = { x = 0.0; y = 0.0 }})
         ; bottom_paddle = bottom_paddle
-                          |> Rect.integrate
-                          |> (fun paddle -> match !input_state with
-                          | { a_pressed = true; d_pressed = false; _ } -> 
-                                  { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
-                          | { a_pressed = false; d_pressed = true; _ } -> 
-                                  { paddle with vel = { paddle.vel with x = paddle_speed } }
-                          | _ -> { paddle with vel = { x = 0.0; y = 0.0 }})
+          |> Rect.integrate
+          |> (fun paddle -> match !input_state with
+              | { a_pressed = true; d_pressed = false; _ } -> 
+                      { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
+              | { a_pressed = false; d_pressed = true; _ } -> 
+                      { paddle with vel = { paddle.vel with x = paddle_speed } }
+              | _ -> { paddle with vel = { x = 0.0; y = 0.0 }})
         ; ball = ball
-                 |> Rect.integrate
-                 |> (fun ball -> if Rect.left ball <. 0.0 || Rect.right ball >. 800.0 
-                                then { ball with vel = { ball.vel with x = ball.vel.x *. -1.0 } }
-                                else ball)
-                 |> (fun ball -> if Rect.overlap ball top_paddle || Rect.overlap ball bottom_paddle
-                                then { ball with vel = { ball.vel with y = ball.vel.y *. -1.0 } <*> 1.1 }
-                                else ball)
-                 |> (fun ball -> if Rect.top ball <. 0.0 || Rect.bottom ball >. 800.0
-                                then { ball with pos = { x = 400.0; y = 400.0 }; vel = random_vel () }
-                                else ball)
-        ; paddle_speed = if Rect.overlap top_paddle ball || Rect.overlap bottom_paddle ball
-                         then paddle_speed *. 1.1 
-                         else if Rect.top ball <. 0.0 || Rect.bottom ball >. 800.0 then 0.0
-                         else paddle_speed }
+          |> Rect.integrate
+          |> (fun ball -> if Rect.left ball <. 0.0 || Rect.right ball >. 800.0 
+                          then { ball with vel = { ball.vel with x = ball.vel.x *. -1.0 } }
+                          else ball)
+          |> (fun ball -> if ball_hit_paddle
+                          then { ball with vel = { ball.vel with y = ball.vel.y *. -1.0 } <*> 1.1 }
+                          else ball)
+          |> (fun ball -> if point_scored
+                          then { ball with pos = { x = 400.0; y = 400.0 }; vel = random_vel () }
+                          else ball)
+        ; paddle_speed = 
+            if ball_hit_paddle then paddle_speed *. 1.1 
+            else if point_scored then 0.0
+            else paddle_speed }
 end
 
 let paddle_bounds = Vector.{x = 90.0; y = 10.0}
+let init_paddle_speed = 4.5
 
+(* Initial Game State *)
 let game_state = ref GameState.{
     top_paddle = Rect.{ 
         pos = Vector.{x = 400.0 -. paddle_bounds.x /. 2.0; y = 10.0}; 
@@ -158,4 +166,4 @@ let () =
         val graphics = graphics
     end;
     let _ = app##.ticker##add update in
-    print_endline "ocaml";
+    ()
