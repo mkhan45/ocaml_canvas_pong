165f4770b67a4e20a6e535265d1f3cc5b236f6fc
Mikail Khan <mikail@mikail-khan.com>
2022-12-17 03:29:36 -0500

    more refactor

--- main.ml
+++ main.ml
@@ -9,22 +9,10 @@ let (>.) = Float.(>)
 let (<.) = Float.(<)
 
 let _ = Random.self_init
+let paddle_bounds = Vector.{x = 100.0; y = 10.0}
+let init_paddle_speed = 4.5
 
-(* Initializes PIXI graphics for drawing primitives *)
-let graphics = 
-    let graphics = new%js graphics () in
-    let _ = graphics##beginFill 0xffffff
-    in graphics
-
-(* Initializes PIXI application *)
-let app = 
-    let app = new%js application object%js val width = 800.0; val height = 800.0 end in
-    let _ = Dom.appendChild Dom_html.document##.body app##.view
-    and _ = app##.stage##addChild(graphics) in
-    app
-
-let _ = Js.Unsafe.global##addEventListener "keyup" Input.handle_keyup
-let _ = Js.Unsafe.global##addEventListener "keydown" Input.handle_keydown
+let (graphics, app) = pixi_setup ()
 
 module Rect = struct
     include Util.Rect
@@ -38,9 +26,6 @@ let random_vel () =
     | r when Float.abs r <. 2.0 -> attempt ()
     | r -> r
     in Vector.{ x = attempt (); y = attempt () } 
-
-let paddle_bounds = Vector.{x = 100.0; y = 10.0}
-let init_paddle_speed = 4.5
 
 module GameState = struct
     type t = { top_paddle: Rect.t; bottom_paddle: Rect.t; ball: Rect.t; paddle_speed: float }
@@ -57,34 +42,44 @@ module GameState = struct
         let ball_hit_paddle = 
             (Rect.overlaps top_paddle ball && ball.vel.y <. 0.0) 
          || (Rect.overlaps bottom_paddle ball && ball.vel.y >. 0.0)
-        and point_scored = Rect.top ball <. 0.0 || Rect.bottom ball >. 800.0 in
+        and point_scored = Rect.top ball <. 0.0 || Rect.bottom ball >. 800.0 
+        in
         { top_paddle = top_paddle
           |> Rect.integrate
           |> (fun paddle -> match !Input.input_state with
           | { left_pressed = true; right_pressed = false; _ } -> 
                 { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
+
           | { left_pressed = false; right_pressed = true; _ } -> 
                 { paddle with vel = { paddle.vel with x = paddle_speed } }
+
           | _ -> { paddle with vel = Vector.zero })
+
         ; bottom_paddle = bottom_paddle
           |> Rect.integrate
           |> (fun paddle -> match !Input.input_state with
               | { a_pressed = true; d_pressed = false; _ } -> 
                       { paddle with vel = { paddle.vel with x = -1.0 *. paddle_speed } }
+
               | { a_pressed = false; d_pressed = true; _ } -> 
                       { paddle with vel = { paddle.vel with x = paddle_speed } }
+
               | _ -> { paddle with vel = Vector.zero })
+
         ; ball = ball
           |> Rect.integrate
           |> (fun ball -> if Rect.left ball <. 0.0 || Rect.right ball >. 800.0 
                           then { ball with vel = { ball.vel with x = ball.vel.x *. -1.0 } }
                           else ball)
+
           |> (fun ball -> if ball_hit_paddle
                           then { ball with vel = { ball.vel with y = ball.vel.y *. -1.0 } <*> 1.1 }
                           else ball)
+
           |> (fun ball -> if point_scored
                           then { ball with pos = { x = 400.0; y = 400.0 }; vel = random_vel () }
                           else ball)
+
         ; paddle_speed = 
             if ball_hit_paddle then paddle_speed *. 1.1 
             else if point_scored then init_paddle_speed
--- pixi_bindings.ml
+++ pixi_bindings.ml
@@ -13,3 +13,15 @@ class type graphics = object
 end
 
 let graphics = Js.Unsafe.global##.PIXI##._Graphics
+
+let pixi_setup () =
+    let graphics = new%js graphics () in
+    let _ = graphics##beginFill 0xffffff in
+
+    let app = new%js application object%js val width = 800.0; val height = 800.0 end in
+    let _ = Dom.appendChild Dom_html.document##.body app##.view
+    and _ = app##.stage##addChild(graphics) in
+
+    let _ = Js.Unsafe.global##addEventListener "keyup" Input.handle_keyup
+    and _ = Js.Unsafe.global##addEventListener "keydown" Input.handle_keydown
+    in (graphics, app)
